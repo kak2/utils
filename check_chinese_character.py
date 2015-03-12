8102fbe7173eecd88aef4290fc264ca7a465678a
baodn <doanngocbao@gmail.com>
2015-03-12 14:17:49 +0700

    check chinese character in files

---
+++ check_chinese_character.py
@@ -0,0 +1,23 @@
+﻿import codecs;
+import os;
+import re;
+
+rootDir = 'D:\ccTalk\Projects\webembedzing';
+for dirName, subdirList, fileList in os.walk(rootDir):
+	for fname in fileList:
+		tmp = fname.split('.')
+		filetype = tmp[len(tmp) - 1]
+		filetypeTarget = ['js', 'css', 'volt', 'php']
+		try:
+			if filetypeTarget.index(filetype) != -1:
+				filename = dirName + '\\' + fname
+				f = codecs.open(filename, 'r', 'utf-8')
+				for line in f:
+					a = re.findall(u'[\u4e00-\u9fff]+',line)
+					if len(a) > 0:
+						print('filename:' + filename)
+						break
+				f.close()
+		except:
+			"Do nothing"
+			
